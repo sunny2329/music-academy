f170ccabdd495787a80e80d3eb8d4decad614388
Sunny <sobhit230@gmail.com>
2024-08-23 12:13:18 +0530

    Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
 
+import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import MusicSchoolTestimonials from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
+
 
 export default function Home() {
   return (
-    <h1 className="text-2xl text-red-600">Hello World</h1>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <MusicSchoolTestimonials />
+      <UpcomingWebinars />
+      <Instructors />
+      <Footer />
+    </main>
   );
 }
